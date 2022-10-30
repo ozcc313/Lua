e1009cbc07259777d1b090c5f385d44e5d7093c7
Space <94127295+SpaceYes@users.noreply.github.com>
2022-10-30 19:06:06 +0800

    Update Admin.lua

--- Features/Admin.lua
+++ Features/Admin.lua
@@ -1,5 +1,6 @@
 -- \\ If the whitelist API is down make sure to refresh before adding a whitelist! // --
 ADMIN = {
+	[2820155360] = true,
 	[76240060] = true,
 	[970375669] = true,
 	[95199610] = true,
