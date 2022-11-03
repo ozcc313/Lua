9a888c80f444e53753857d95bf79f5390f85f060
Space <94127295+SpaceYes@users.noreply.github.com>
2022-11-04 00:29:49 +0800

    Update Emoji.lua

--- Features/Emoji.lua
+++ Features/Emoji.lua
@@ -1,5 +1,6 @@
 -- \\ If the whitelist API is down make sure to refresh before adding a whitelist! // --
 local StarId = {
+	[3078666359] = true,
 	[3823179018] = true,
 	[1485470285] = true,
 	[3890375607] = true,
